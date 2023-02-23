807bcc14db8e98debf4fc80671356aff7078ed5a
RathinasamyAnnaUniversity <126123188+RathinasamyAnnaUniversity@users.noreply.github.com>
2023-02-23 05:35:57 +0530

    Add files via upload

---
+++ nindex.py
@@ -0,0 +1,16 @@
+<! DOC TYPE html>
+< html lang = "en">
+< head >
+    < meta charset = "UTF-8" >
+    < meta http-equiv = "X-UA-compatible" content = "IE=edge" >
+    < meta name ="viewport" content = "with=device-width,initial-scale= 1.0" >
+    < title > Home </title >
+</head>
+<body>
+    <h1> A Music Management Sharing Streaming platform <h1>
+    <form method = 'POST'>
+        {{form.hidden_tag()}}
+        {{form.file()}}
+        {{form.sumit()}}
+</body>
+</html>
